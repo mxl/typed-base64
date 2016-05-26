a0090d62f02663621aa2979e0d32fcabc5db1ef9
Michal Ledin <m.ledin@appheads.ru>
2016-05-27 01:07:07 +0300

    Added return types.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare namespace Base64Js {
-    function toByteArray(b64: string);
+    function toByteArray(b64: string):Uint8Array;
 
-    function fromByteArray(uint8: Uint8Array);
+    function fromByteArray(uint8: Uint8Array):string;
 }
 
 export = Base64Js
