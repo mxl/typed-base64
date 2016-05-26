dc9b9fcdf4d1c3fdb5b3d138c5a5c12884b24ee4
Michal Ledin <m.ledin@appheads.ru>
2016-05-26 23:58:57 +0300

    Added definition.

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare namespace Base64Js {
+    function toByteArray(b64: string);
+
+    function fromByteArray(uint8: Uint8Array);
+}
+
+export = Base64Js
---
+++ test.ts
@@ -0,0 +1,14 @@
+import {toByteArray, fromByteArray} from 'base64-js';
+import * as test from 'tape';
+
+let b64 = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz\
+IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg\
+dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu\
+dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo\
+ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+
+test('a test', (t) => {
+    t.equal(fromByteArray(toByteArray(b64)), b64);
+    t.end();
+});
+
